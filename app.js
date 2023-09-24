9a5ca13576c8234d5c99f1aec7abcf7b87a5d8e8
Om Prakash Bairwa <op1999mina@gmail.com>
2023-09-24 20:11:14 +0530

    [ADDED] Cors Package (#4) (#5)
    Co-authored-by: Mradul Singh <100354790+mradulsingh0223@users.noreply.github.com>

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ import gameRouter from "./routes/gameRouter.js";
 import { errorMiddleware } from "./middlewares/error.js";
 import swaggerUi from "swagger-ui-express";
 import YAML from 'yamljs';
-
+import cors from 'cors'
 
 const swaggerDocument = YAML.load('./config/gameRoutes.yaml');
 export const app = express();
+app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
