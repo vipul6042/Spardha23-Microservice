cbf55a04f6ee0786018f21fc1fcaa42817ac9c30
Varun-Kolanu <112728411+Varun-Kolanu@users.noreply.github.com>
2023-10-10 23:08:24 +0530

    [ADDED] Checking of user is staff or admin in adding, editing, deleting games (#6)
    * [ADDED] Checking of user is staff or admin in adding, editing, deleting games
    
    * [UPDATED] Swagger
    
    * Added Cors
    
    ---------
    
    Co-authored-by: Mradul Singh <mradul.singh.min21@itbhu.ac.in>

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from "express";
 import gameRouter from "./routes/gameRouter.js";
 import { errorMiddleware } from "./middlewares/error.js";
 import swaggerUi from "swagger-ui-express";
+import cors from 'cors';
 import YAML from 'yamljs';
-import cors from 'cors'
 
 const swaggerDocument = YAML.load('./config/gameRoutes.yaml');
 export const app = express();
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import { app } from "./app.js";
 import { connectDB } from "./database/database.js";
 import { config } from "dotenv";
 import { logDiscord } from "./utils/logDiscord.js";
+import { connectPgDB } from "./database/userDatabase.js";
 config();
 
 const server=app.listen(process.env.PORT, () => {
 	connectDB();
+	connectPgDB();
 	console.log("listening on port " + process.env.PORT);
 });
 
