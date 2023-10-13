4e2c2f90e03a78d4fe4b8ffb681c71a9c0b1965d
Mradul Singh <mradul.singh.min21@itbhu.ac.in>
2023-10-13 11:12:16 +0530

    [UPDATED] pg connection (#9)
    * [ADDED] health end point
    
    * [UPDATED] pg connection

--- database/userDatabase.js
+++ database/userDatabase.js
@@ -10,6 +10,7 @@ export const pool = new Pool({
     "database": process.env.DB_DATABASE,
     "password": process.env.DB_PASSWORD,
     "port": process.env.DB_PORT,
+    "ssl": true
 })
 
 export const connectPgDB = () => {
